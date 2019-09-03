8ac7aaf93cad2b14ff8d45fc6157af2a5a63b1df
zhangjie520 <2521544167@qq.com>
2019-09-03 10:13:34 +0800

    add github login fuction

---
+++ src/main/java/com/example/community/dto/AccessTokenDTO.java
@@ -0,0 +1,59 @@
+package com.example.community.dto;
+
+//import lombok.Data;
+//
+//@Data
+public class AccessTokenDTO {
+//    client_id	string	Required. The client ID you received from GitHub for your GitHub App.
+//    client_secret	string	Required. The client secret you received from GitHub for your GitHub App.
+//    code	string	Required. The code you received as a response to Step 1.
+//    redirect_uri	string	The URL in your application where users are sent after authorization.
+//    state	string	The unguessable random string you provided in Step 1.
+    private String client_id;
+    private String client_secret;
+    private String code;
+
+    public String getClient_id() {
+        return client_id;
+    }
+
+    public void setClient_id(String client_id) {
+        this.client_id = client_id;
+    }
+
+    public String getClient_secret() {
+        return client_secret;
+    }
+
+    public void setClient_secret(String client_secret) {
+        this.client_secret = client_secret;
+    }
+
+    public String getCode() {
+        return code;
+    }
+
+    public void setCode(String code) {
+        this.code = code;
+    }
+
+    public String getRedirect_uri() {
+        return redirect_uri;
+    }
+
+    public void setRedirect_uri(String redirect_uri) {
+        this.redirect_uri = redirect_uri;
+    }
+
+    public String getState() {
+        return state;
+    }
+
+    public void setState(String state) {
+        this.state = state;
+    }
+
+    private String redirect_uri;
+    private String state;
+
+}
---
+++ src/main/java/com/example/community/provider/GithubProvider.java
@@ -0,0 +1,46 @@
+package com.example.community.provider;
+
+import com.alibaba.fastjson.JSON;
+import com.example.community.dto.AccessTokenDTO;
+import com.example.community.dto.GithubUser;
+import okhttp3.*;
+import org.springframework.stereotype.Component;
+
+import java.io.IOException;
+
+@Component
+public class GithubProvider {
+    public String getAccessToken(AccessTokenDTO accessTokenDTO){
+        MediaType mediaType = MediaType.get("application/json; charset=utf-8");
+
+        OkHttpClient client = new OkHttpClient();
+
+        RequestBody body = RequestBody.create(mediaType, JSON.toJSONString(accessTokenDTO));
+        Request request = new Request.Builder()
+                .url("https://github.com/login/oauth/access_token")
+                .post(body)
+                .build();
+        try (Response response = client.newCall(request).execute()) {
+            String string =response.body().string();
+            String token = string.split("&")[0].split("=")[1];
+            return token;
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return null;
+    }
+    public GithubUser getUser(String accessToken){
+        OkHttpClient client = new OkHttpClient();
+        Request request = new Request.Builder()
+                .url("https://api.github.com/user?access_token="+accessToken)
+                .build();
+
+        try (Response response = client.newCall(request).execute()) {
+            String string=response.body().string();
+            return JSON.parseObject(string,GithubUser.class);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return null;
+    }
+}
