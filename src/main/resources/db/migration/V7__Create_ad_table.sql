4ae002d0a8dd64cd7edf81e06ccd476f99d97381
zhangjie520 <2521544167@qq.com>
2019-10-23 21:13:02 +0800

    在分支上添加ad表

---
+++ src/main/resources/db/migration/V7__Create_ad_table.sql
@@ -0,0 +1,13 @@
+create table ad
+(
+    id bigint auto_increment primary key,
+    title varchar(256) default null ,
+    url varchar(512) default null,
+    img varchar(256) default null,
+    gmt_start bigint,
+    gmt_end bigint,
+    gmt_create bigint,
+    gmt_modify bigint,
+    status int,
+    pos varchar(10) not null
+);
