a020039392f6417c2cdf04443a747a2032640afc
zhangjie520 <2521544167@qq.com>
2019-10-26 15:38:34 +0800

    修改了ad表的image为img

--- src/main/resources/db/migration/V7__Create_ad_table.sql
+++ src/main/resources/db/migration/V7__Create_ad_table.sql
@@ -3,7 +3,7 @@ create table ad
     id bigint auto_increment primary key,
     title varchar(256) default  null ,
     url varchar(512) default  null ,
-    image varchar(256) default null,
+    img varchar(256) default null,
     gmt_start bigint,
     gmt_end bigint,
     gmt_create bigint,
