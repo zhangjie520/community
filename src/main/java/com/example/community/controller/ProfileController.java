8c639ce1cf6da0ff4224a9dc01efd0c85838f88b
zhangjie520 <2521544167@qq.com>
2019-10-29 17:27:22 +0800

    格式化了profilecontroller文件

--- src/main/java/com/example/community/controller/ProfileController.java
+++ src/main/java/com/example/community/controller/ProfileController.java
@@ -2,9 +2,7 @@ package com.example.community.controller;
 
 import com.example.community.dto.NotificationDTO;
 import com.example.community.dto.PaginationDTO;
-import com.example.community.mapper.NotificationMapper;
 import com.example.community.mapper.UserMapper;
-import com.example.community.model.Notification;
 import com.example.community.model.User;
 import com.example.community.service.NotificationService;
 import com.example.community.service.QuestionService;
@@ -16,9 +14,7 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestParam;
 
 import javax.annotation.Resource;
-import javax.servlet.http.Cookie;
 import javax.servlet.http.HttpServletRequest;
-import java.util.List;
 import java.util.stream.Collectors;
 
 @Controller
@@ -33,31 +29,31 @@ public class ProfileController {
     @GetMapping("/profile/{action}")
     public String profile(@PathVariable("action") String action,
                           Model model,
-                          @RequestParam(value = "page",defaultValue = "1") Integer pageIndex,
-                          @RequestParam(value = "size",defaultValue = "5") Integer  size,
-                          HttpServletRequest request){
-        User user=(User)request.getSession().getAttribute("user");
+                          @RequestParam(value = "page", defaultValue = "1") Integer pageIndex,
+                          @RequestParam(value = "size", defaultValue = "5") Integer size,
+                          HttpServletRequest request) {
+        User user = (User) request.getSession().getAttribute("user");
         if (user == null) {
 //            request.getSession().setAttribute("error","用户未登录");
             model.addAttribute("error", "用户未登录");
             return "redirect:/";
         }
-        if ("questions".equals(action)){
-            model.addAttribute("section","questions");
-            model.addAttribute("sectionName","我的提问");
+        if ("questions".equals(action)) {
+            model.addAttribute("section", "questions");
+            model.addAttribute("sectionName", "我的提问");
 
-            //将查询到的我的question放入model
-            PaginationDTO paginationDTOS =questionService.listByUserId(user.getId(),pageIndex,size);
-            model.addAttribute("paginationS",paginationDTOS);
+            // 将查询到的我的question放入model
+            PaginationDTO paginationDTOS = questionService.listByUserId(user.getId(), pageIndex, size);
+            model.addAttribute("paginationS", paginationDTOS);
         }
-        if ("replies".equals(action)){
-            model.addAttribute("section","replies");
-            model.addAttribute("sectionName","最新回复");
+        if ("replies".equals(action)) {
+            model.addAttribute("section", "replies");
+            model.addAttribute("sectionName", "最新回复");
 
             PaginationDTO<NotificationDTO> paginationDTO = notificationService.listByReceiver(user, pageIndex, size);
             int unreadCount = paginationDTO.getData().stream().filter(notificationDTO -> notificationDTO.getStatus() == 0).collect(Collectors.toList()).size();
-            model.addAttribute("paginationS",paginationDTO);
-            model.addAttribute("unreadCount",unreadCount);
+            model.addAttribute("paginationS", paginationDTO);
+            model.addAttribute("unreadCount", unreadCount);
         }
 
         return "profile";
